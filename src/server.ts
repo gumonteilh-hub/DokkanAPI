8795df892a1c0ef29e21ef0afb62996c4f243876
gumonteilh <guillaumemont@outlook.fr>
2024-10-13 22:49:16 +0200

    add leadedBy feature

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ let express = require('express');
 let cors = require('cors');
 let { graphqlHTTP } = require('express-graphql');
 var graphql = require('graphql');
-import { GraphQLList, GraphQLString, GraphQLBoolean } from "graphql";
-import { Character } from "./model";
+import { GraphQLInt, GraphQLList, GraphQLString } from "graphql";
+import { Card, Character } from "./model";
 import { cleanData, isInCategories } from "./utils";
 
 // prepare data 
@@ -14,13 +14,12 @@ const data = cleanData(Brutdata);
 var imageType = new graphql.GraphQLObjectType({
   name: 'Image',
   fields: {
-    simpleImage: { type: GraphQLBoolean! },
     simpleUrl: { type: GraphQLString },
     complexeUrl: { type: GraphQLString },
   }
 })
 
-var characterLiteType = new graphql.GraphQLObjectType({
+var CardType = new graphql.GraphQLObjectType({
   name: 'Card',
   fields: {
     id: {
@@ -33,37 +32,113 @@ var characterLiteType = new graphql.GraphQLObjectType({
     type: { type: GraphQLString },
     imageURL: { type: imageType },
     leaderSkill: { type: GraphQLString },
-    ezaLeaderSkill: { type: GraphQLString },
     superAttack: { type: GraphQLString },
   }
 });
+
+var LeadedCards = new graphql.GraphQLObjectType({
+  name: 'LeadedCards',
+  fields: {
+    boostPercentage: { type: GraphQLInt! },
+    cards: { type: GraphQLList(CardType) },
+  }
+})
 
 // query
 var queryType = new graphql.GraphQLObjectType({
   name: 'Query',
   fields: {
     characters: {
-      type: new GraphQLList(characterLiteType),
+      type: new GraphQLList(CardType),
       args: {
-        categories: { type: graphql.GraphQLList(graphql.GraphQLString) },
+        categories: { type: GraphQLList(GraphQLString) },
+        id: { type: GraphQLString }
       },
       // @ts-ignore
-      resolve: (_notUsed, {categories}) => {
+      resolve: (_notUsed, { categories, id }) => {
+
+        if (id) {
+          return [data.find(card => card.id === id)]
+        }
+
         return data.filter(character => isInCategories(character, categories))
       }
     },
     lastCharacters: {
-      type: new GraphQLList(characterLiteType),
+      type: new GraphQLList(CardType),
       resolve: () => {
         return data.slice(0, 100);
       }
     },
     allCharacters: {
-      type: new GraphQLList(characterLiteType),
+      type: new GraphQLList(CardType),
       resolve: () => {
         return data;
       }
-    }
+    },
+    charactersLeadBy: {
+      type: new GraphQLList(LeadedCards),
+      args: {
+        id: { type: GraphQLString },
+      },
+      // @ts-ignore
+      resolve: (_notUsed, { id }) => {
+        const leader = data.find(card => card.id === id)?.technicalLeaderSkill;
+        if (!leader?.mainLeaders) return [];
+
+        let boostGroups: {
+          [key: number]: Card[]
+        } = {}
+
+        // Parcourir toutes les cartes
+        for (let card of data) {
+          let mainLeaderBoost = null;
+          let secondaryLeaderBoost = null;
+
+          // Chercher le leader principal applicable à la carte
+          for (let mainLeader of leader.mainLeaders) {
+            if (card.categories.has(mainLeader.category)) {
+              // Comparer pour garder le leader principal avec le plus haut pourcentage
+              const currentBoost = mainLeader.percentage;
+              if (!mainLeaderBoost || currentBoost > mainLeaderBoost) {
+                mainLeaderBoost = currentBoost;
+              }
+            }
+          }
+
+          // Si un leader principal a été trouvé, chercher un leader secondaire applicable
+          if (mainLeaderBoost !== null) {
+            for (let secondaryLeader of leader.secondaryLeaders) {
+              if (card.categories.has(secondaryLeader.category)) {
+                secondaryLeaderBoost = secondaryLeader.percentage;
+                break;  // On suppose qu'il n'y a qu'un leader secondaire applicable
+              }
+            }
+
+            // Calcul du pourcentage total de boost
+            let totalBoost = mainLeaderBoost;
+            if (secondaryLeaderBoost) {
+              totalBoost += secondaryLeaderBoost;
+            }
+
+            // Ajouter la carte avec son boost
+            // Regrouper les cartes par pourcentage
+            if (!boostGroups[totalBoost]) {
+              boostGroups[totalBoost] = [];
+            }
+            boostGroups[totalBoost].push(card);
+          }
+        }
+
+        const result = Object.entries(boostGroups).map(([boostPercentage, cards]) => ({
+          boostPercentage : parseInt(boostPercentage),
+          cards
+        })).sort((a,b) => (b.boostPercentage - a.boostPercentage));
+
+        return result;
+      }
+
+    },
   }
 });
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,10 @@
-import { Card, Character } from "./model";
+import { Card, Category, Character, Lead, LeaderRecap } from "./model";
 
 export function isInCategories(card: Card, categories: string[]) {
     // character needs to be in every category given
     return categories.every(requiredCategorie => {
         // only one category needs to match the search category
-        return card.categories.some(cardCategorie => {
-            // may change this to be exact match rather than includes. Seems more flexible right now
-            return requiredCategorie.toLowerCase().includes(cardCategorie.toLowerCase())
-        })
+        return card.categories.has(requiredCategorie)
     });
 }
 
@@ -15,17 +12,20 @@ export function cleanData(Brutdata: Character[]): Card[] {
     const liteData: Card[] = Brutdata.map(character => (mapToCard(character)));
 
     const sortedData = liteData.sort((a, b) => parseInt(b.id) - parseInt(a.id))
-    return sortedData.filter(char => {
-        const evolvedId = (parseInt(char.id) + 1).toString();
+    const filteredData = sortedData.filter((card, index) => {
+        const evolvedId = (parseInt(card.id) + 1);
         for (const character of sortedData) {
-            if (character.id == evolvedId) {
+            const charId = parseInt(character.id)
+            if (charId === evolvedId) {
                 return false
             }
-            if (character.id < evolvedId)
+            if (charId < evolvedId)
                 return true;
         }
         return true
     })
+
+    return filteredData;
 }
 
 function mapToCard(character: Character): Card {
@@ -37,9 +37,74 @@ function mapToCard(character: Character): Card {
         type: character.type,
         imageURL: character.imageURL,
         leaderSkill: character.ezaActiveSkill ?? character.leaderSkill,
-        categories: character.categories
+        technicalLeaderSkill: parseLeaders(character.ezaActiveSkill ?? character.leaderSkill),
+        categories: new Set(character.categories)
     }
 }
 
+function parseLeaders(leaderAsString: string): LeaderRecap {
+    // Regex pour capturer le leader principal (avant "plus an additional")
+    const mainLeaderRegex = /"(.*?)"(?:, "(.*?)")?(?: or "(.*?)")? Category Ki \+(\d+) and HP, ATK & DEF \+(\d+)%/;
+    // Regex pour capturer le leader secondaire (après "plus an additional")
+    const secondaryLeaderRegex = /plus an additional HP, ATK & DEF \+(\d+)% for characters who also belong to the "(.*?)"(?:, "(.*?)")?(?: or "(.*?)")? Category/;
+    // Regex pour capturer la catégorie "Super Class"
+    // const superClassRegex = /Super Class Ki \+(\d+) and HP, ATK & DEF \+(\d+)%/;
+    // const extremeClassRegex = /Extreme Class Ki \+(\d+) and HP, ATK & DEF \+(\d+)%/;
 
+    // Recherche du leader principal
+    const mainLeaderMatch = mainLeaderRegex.exec(leaderAsString);
+    const secondaryLeaderMatch = secondaryLeaderRegex.exec(leaderAsString);
 
+    let mainLeaders: Lead[] = [];
+    let secondaryLeaders: Lead[] = [];
+    // let alternativeLeaders: Lead[] = [];
+
+    // Si un leader principal a été trouvé, on associe chaque catégorie avec son pourcentage
+    if (mainLeaderMatch) {
+        const ki = mainLeaderMatch[4];
+        const percentage = mainLeaderMatch[5];
+        mainLeaders = mainLeaderMatch.slice(1, 4).filter(Boolean).map(category => ({
+            category: parseCategory(category),
+            ki: parseInt(ki),
+            percentage: parseInt(percentage)
+        }));
+    }
+
+    // Si un leader secondaire a été trouvé, on associe chaque catégorie avec son pourcentage (ici toujours 30%)
+    if (secondaryLeaderMatch) {
+        const percentage = secondaryLeaderMatch[1];
+        secondaryLeaders = secondaryLeaderMatch.slice(2, 5).filter(Boolean).map(category => ({
+            category: parseCategory(category),
+            percentage: parseInt(percentage)
+        }));
+    }
+
+    // Si un Super Class leader a été trouvé, on l'ajoute en tant que leader alternatif
+    // if (superClassMatch) {
+    //     const ki = superClassMatch[1];
+    //     const percentage = superClassMatch[2];
+    //     alternativeLeaders.push({
+    //         category: parseCategory("Super Class"),
+    //         ki: parseInt(ki),
+    //         percentage: parseInt(percentage)
+    //     });
+    // }
+
+    return {
+        mainLeaders: mainLeaders,
+        secondaryLeaders: secondaryLeaders,
+        // alternativeLeaders: alternativeLeaders
+    };
+}
+
+function parseCategory(categoryName: string): Category {
+    const categoryKey = Object.keys(Category).find(
+        key => Category[key as keyof typeof Category] === categoryName
+    );
+    if (categoryKey) {
+        return Category[categoryKey as keyof typeof Category];
+    } else {
+        return Category.Unsuported;
+    }
+
+}
